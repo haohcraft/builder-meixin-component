0e0e747a5cedc3a878eec2734b95a2795ecb555b
Hao Huang <hhuang@dataminr.com>
2015-11-16 22:33:55 -0500

    build by pages

--- config/webpack/webpack.config.pages.js
+++ config/webpack/webpack.config.pages.js
@@ -2,13 +2,21 @@
 var path = require("path");
 var webpack = require("webpack");
 var config = require("./webpack.config");
+var readDir = require('readdir');
+var _ = require('lodash');
 
 // Replace with `__dirname` if using in project root.
 var ROOT = process.cwd();
-var req = require.context(path.join(ROOT, "src/pages"), false, /^\.\/.*\.js$/);
-
-config.entry = req.keys().map(function(k) {
-	return path.join(ROOT, "src/pages", k); 
-});
+var req = readDir.readSync(path.join(ROOT, "src/pages"), ['*.js']);
+config.entry = _.reduce(req, function(result, n) {
+    var reg = n.match(/(\S+).js/i);
+    if(reg && reg.length) {
+        result[reg[1]] = path.join(ROOT, "src/pages", n);
+    }
+    return result;
+}, {});
+config.entry.vendor = ['react', 'react-dom', 'lodash', 'moment'];
+config.output.path = path.join(ROOT, "dist/pages/");
+config.plugins.push(new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.js"));
 // Export mutated base.
 module.exports = config;
