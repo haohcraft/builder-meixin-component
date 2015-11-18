0fefc90c5798b9ba84bfcbf4c33b2eb37d005101
Hao Huang <hhuang@dataminr.com>
2015-11-18 10:37:27 -0500

    added logic to extract css

--- config/webpack/webpack.config.pages.js
+++ config/webpack/webpack.config.pages.js
@@ -2,6 +2,7 @@
 var path = require("path");
 var webpack = require("webpack");
 var config = require("./webpack.config");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var readDir = require('readdir');
 var _ = require('lodash');
 
@@ -17,6 +18,27 @@ config.entry = _.reduce(req, function(result, n) {
 }, {});
 config.entry.vendor = ['react', 'react-dom', 'lodash', 'moment'];
 config.output.path = path.join(ROOT, "dist/pages/");
+config.module.loaders = [
+    {
+        test: /\.js?$/,
+        exclude: [/node_modules/],
+        // **Note**: Cannot use shorthand `"babel-loader"` or `"babel"` when
+        // we are playing around with `NODE_PATH` in builder. Manually
+        // resolve path.
+        loader: require.resolve("babel-loader")
+    },  { 
+        test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer?browsers=last 2 version!sass') 
+    }, {
+        test: /\.(png|jpg)$/,
+        loader: require.resolve("url-loader") + "?limit=8192"
+    }, {
+        test: /\.json$/,
+        loader: require.resolve("json-loader")
+    }
+
+];
+   
+config.plugins.push(new ExtractTextPlugin('[name].min.css'));
 config.plugins.push(new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.js"));
 // Export mutated base.
 module.exports = config;
